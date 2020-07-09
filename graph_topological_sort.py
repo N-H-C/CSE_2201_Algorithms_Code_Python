3229e0cda5860e7bc9021ebd63113c6a4adf7af8
N-H-C <nakib2025@gmail.com>
2020-07-09 21:36:25 +0600

    first commit

--- graph_topological_sort.py
+++ graph_topological_sort.py
@@ -1,7 +1,7 @@
 import queue
 
 
-def dfs(adj, visited, current_node):
+def dfs(adj, visited, current_node, stack):
     if visited[current_node] != 0:
         return
 
@@ -10,25 +10,46 @@ def dfs(adj, visited, current_node):
     for i in range(len(adj[current_node])):  # not a pythonic way
         adjacent_node = adj[current_node][i]
         if visited[adjacent_node] == 0:
-            dfs(adj, visited, adjacent_node)
+            dfs(adj, visited, adjacent_node, stack)
 
-    print(current_node, end=' ')
+    stack.put(current_node)
 
 
 n = int(input("Enter the number of nodes: "))
 e = int(input("Enter the number of edges: "))
 
+adj_list = [[] for i in range(n)]
+
 print("Enter edges in separate lines.")
 for i in range(e):
     u, v = map(int, input().split())
     adj_list[u].append(v)
-    adj_list[v].append(u)
+    #adj_list[v].append(u)
 
-
-print(adj_list)
+stack = queue.LifoQueue(maxsize=n)
 
 visited = [0 for i in range(n)]
-print('DFS: ', end="")
 for i in range(n):
     if visited[i] == 0:
-        dfs(adj_list, visited, i)
+        dfs(adj_list, visited, i, stack)
+
+
+print('Topological order: ', end="")
+while not stack.empty():
+    node = stack.get()
+    print(node, end=' ')
+
+
+"""
+Sample input / output
+Enter the number of nodes: 6
+Enter the number of edges: 6
+Enter edges in separate lines.
+5 2
+5 0
+4 0
+4 1
+2 3
+3 1
+DFS: 5 4 2 3 1 0
+"""
